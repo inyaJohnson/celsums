45896d373348ede8df565a3302020c2ec9ceac08
inyaJohnson <intoajohnson@gmail.com>
2022-01-30 23:05:37 +0100

    view scaffold

---
+++ resources/views/events/show.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.template')
+@section('content')
+    <main class="main">
+        <section class="promo-primary">
+            <picture>
+                <source srcset="img/event-details.jpg" media="(min-width: 992px)" /><img class="img--bg"
+                    src="img/event-details.jpg" alt="img" />
+            </picture>
+            <div class="promo-primary__description"> <span>Happiness</span></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-auto">
+                        <div class="align-container">
+                            <div class="align-container__item"><span class="promo-primary__pre-title">Helpo</span>
+                                <h1 class="promo-primary__title"><span>Bike</span> <span>Racing</span></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- article start-->
+        <section class="section article"><img class="article__bg" src="img/events_inner-bg.png" alt="img" />
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10 offset-lg-1 col-xl-8 offset-xl-2">
+                        <div class="article__img"><img src="img/article.jpg" alt="img" /></div>
+                        <p><strong>Thresher shark rudd African lungfish silverside, Red salmon rockfish grunion, garpike
+                                zebra danio king-of-the-salmon banjo catfish. Sea chub demoiselle whalefish zebra lionfish
+                                mud cat pelican eel.</strong></p>
+                        <p>Minnow snoek icefish velvet-belly shark, California halibut round stingray northern sea robin.
+                            Southern grayling trout-perch. Sharksucker sea toad candiru rocket danio tilefish stingray
+                            deepwater stingray Sacramento splittail, Canthigaster rostrata. Midshipman dartfish Modoc
+                            sucker, yellowtail kingfish basslet. Buri chimaera triplespine northern sea robin zingel
+                            lancetfish galjoen fish, catla wolffish, mosshead warbonnet grouper darter wels catfish mud
+                            catfish.</p>
+                        <p class="article__text-filled text-filled">Streamer fish California halibut Pacific saury.
+                            Slickhead grunion lake trout. Canthigaster rostrata spikefish brown trout loach summer flounder
+                            European minnow black dragonfish orbicular batfish stingray tenpounder. Sucker lionfish
+                            garibaldi surgeonfish</p>
+                        <p>African lungfish silverside, Red salmon rockfish grunion, garpike zebra danio king-of-the-salmon
+                            banjo catfish. Sea chub demoiselle whalefish zebra lionfish mud cat pelican eel. Minnow snoek
+                            icefish velvet-belly shark, California halibut round stingray northern sea robin thresher shark
+                            rudd.</p>
+                        <p>Southern grayling trout-perch. Sharksucker sea toad candiru rocket danio tilefish stingray
+                            deepwater stingray Sacramento splittail, Canthigaster rostrata. Midshipman dartfish Modoc
+                            sucker, yellowtail</p>
+                        <ul class="unordered-list article__list">
+                            <li>Streamer fish California halibut Pacif Slickhead grunion lake trout</li>
+                            <li>Canthigaster rostrata spikefish Brown trout loach summer flounder European minnow black</li>
+                            <li>Orbicular batfish stingray tenpounder. Sucker lionfish garibaldi surgeonfish </li>
+                            <li>Celebes rainbowfish forehead brooder, mudskipper barred danio bat ray bighead carp</li>
+                            <li>Tripod fish rudd, mouthbrooder pirate perch pencil catfish lanternfish, whiptail gulper
+                                chain pickerel </li>
+                            <li>Mud catfish piranha skilfish warty angler North American darter bonnetmouth beaked salmon
+                            </li>
+                        </ul>
+                        <p>Burma danio black bass straptail southern Dolly Varden orbicular velvetfish trumpetfish;
+                            bluntnose minnow. Hatchetfish pricklefish sixgill ray sawfish scaly dragonfish! Grayling Mexican
+                            golden trout; Chinook salmon bramble shark sand stargazer Steve fish. Scat zebra pleco
+                            graveldiver river shark tripod fish; flagtail bala shark warbonnet. Hatchetfish pricklefish
+                            sixgill ray sawfish scaly dragonfish! Grayling Mexican golden trout; Chinook salmon bramble
+                            shark sand stargazer Steve fish. Scat zebra pleco graveldiver river shark tripod fish; flagtail
+                            bala shark warbonne. Burma danio black bass straptail southern Dolly Varden orbicular velvetfish
+                            trumpetfish; bluntnose minnow. </p>
+                        <div class="article-information">
+                            <div class="row offset-30">
+                                <div class="col-sm-6 col-lg-4">
+                                    <div class="article-information__item" style="background: #32C876;">
+                                        <h6 class="article-information__title">Details</h6>
+                                        <div class="article-information__details">
+                                            <div class="article-information__details-item">
+                                                <span>Start:</span><span>September 30’ 19</span></div>
+                                            <div class="article-information__details-item"><span>Finish:</span><span>October
+                                                    05’ 19</span></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-lg-4">
+                                    <div class="article-information__item" style="background: #F76588;">
+                                        <h6 class="article-information__title">Organizer</h6>
+                                        <div class="article-information__details">
+                                            <div class="article-information__details-item"><span>Phone:</span><a
+                                                    href="tel:+31859644725">+31 85 964 47 25</a></div>
+                                            <div class="article-information__details-item"><span>E-mail:</span><a
+                                                    href="mailto:helpo@gmail.com">helpo@gmail.com</a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-lg-4">
+                                    <div class="article-information__item" style="background: #49C2DF;">
+                                        <h6 class="article-information__title">Venue</h6>
+                                        <div class="article-information__details">
+                                            <div class="article-information__details-item"><span>Location:</span><span>Dark
+                                                    Spurt, San Francisco, CA 94528</span></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- article end-->
+        <!-- additional events start-->
+        <section class="section additional-events no-padding-top no-padding-bottom">
+            <div class="row no-gutters">
+                <div class="col-md-6">
+                    <div class="additional-event"><a class="additional-event__img" href="event-details.html"><img
+                                class="img--bg" src="img/additional_1.jpg" alt="img" />
+                            <div class="additional-event__text"><span>The Culture of</span><span>India. Volunteer</span>
+                            </div>
+                        </a></div>
+                </div>
+                <div class="col-md-6">
+                    <div class="additional-event"><a class="additional-event__img" href="event-details.html"><img
+                                class="img--bg" src="img/additional_2.jpg" alt="img" />
+                            <div class="additional-event__text"><span>Trinity</span><span>Trash Bash</span></div>
+                        </a></div>
+                </div>
+            </div>
+        </section>
+        <!-- additional events end-->
+    </main>
+@endsection
---
+++ resources/views/teams/show.blade.php
@@ -0,0 +1,222 @@
+@extends('layouts.template')
+@section('content')
+    <main class="main">
+        <section class="promo-primary">
+            <picture>
+                <source srcset="img/volunteer.jpg" media="(min-width: 992px)" /><img class="img--bg"
+                    src="img/volunteer.jpg" alt="img" />
+            </picture>
+            <div class="promo-primary__description"> <span>Compassion</span></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-auto">
+                        <div class="align-container">
+                            <div class="align-container__item"><span class="promo-primary__pre-title">Helpo</span>
+                                <h1 class="promo-primary__title"><span>Team</span> <span>Member</span></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- section start-->
+        <section class="section team-member no-padding-bottom">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-xl-5">
+                        <div class="img-box"><img class="img--layout" src="img/about_layout-reverse.png"
+                                alt="img" />
+                            <div class="img-box__img"><img class="img--bg" src="img/img_box-1.jpg" alt="img" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-6 offset-xl-1">
+                        <div class="heading heading--primary"><span class="heading__pre-title">Community a Officer – Mr
+                                Charity</span>
+                            <h2 class="heading__title"><span>Eey Chie</span></h2>
+                            <p>Alienum phaedrum torquatos nec eu, vis detraxit periculis ex, nihil expetendis in mei. Mei an
+                                pericula euripidis, hinc partem ei est. Eos ei nisl graecis, vix aperiri consequat an. Eius
+                                lorem tincidunt vix at, vel pertinax sensibus id, error epicurei mea et. Mea facilisis
+                                urbanitas moderatius id. Vis ei rationibus definiebas, eu qui purto zril laoreet. Ex error
+                                omnium interpretaris pro, alia illum ea vim. Lorem ipsum dolor sit amet, eu per legimus
+                                referrentur. Ius ne viris repudiare, nominavi sententiae eos in.</p>
+                            <p>Alienum phaedrum torquatos nec eu, vis detraxit periculis ex, nihil expetendis in mei. Mei an
+                                pericula euripidis, hinc partem ei est. Eos ei nisl graecis, vix aperiri consequat an. Eius
+                                lorem tincidunt vix at, vel pertinax sensibus id, error epicurei mea et. Mea facilisis
+                                urbanitas moderatius id. Vis ei rationibus definiebas.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section team-member">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-xl-5">
+                        <div class="team-member__skills">
+                            <h4 class="team-member__title">My Skills</h4>
+                            <p class="margin-bottom">Alienum phaedrum torquatos nec eu, vis detraxit periculis ex, nihil
+                                expetendis in mei. Mei an pericula euripidis, hinc partem ei est. Eos ei nisl graecis, vix
+                                aperiri consequat an. Eius lorem tincidunt vix at, vel pertinax. Mei an pericula euripidis,
+                                hinc partem ei est. , hinc partem ei est.</p>
+                            <h6 class="team-member__subtitle">Leadership</h6>
+                            <div class="progress-bar">
+                                <div class="progress-bar__inner" style="width: 78%;">
+                                    <div class="progress-bar__value">78%</div>
+                                </div>
+                            </div>
+                            <h6 class="team-member__subtitle">Volunteering</h6>
+                            <div class="progress-bar">
+                                <div class="progress-bar__inner" style="width: 72%;">
+                                    <div class="progress-bar__value">72%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-6 offset-xl-1">
+                        <div class="team-member__details">
+                            <h4 class="team-member__title">Contact details</h4>
+                            <div class="team-member__details-item"><span>Phone:</span><span><a href="tel:+100026655787">+
+                                        1000 266 55787</a></span></div>
+                            <div class="team-member__details-item"><span>Email:</span><span><a
+                                        href="mailto:donate.d@charity.com">donate.d@charity.com</a></span></div>
+                            <div class="team-member__details-item"><span>Birthday:</span><span>September 21, 1992</span>
+                            </div>
+                            <div class="team-member__details-item"><span>Location:</span><span>Nursultan</span></div>
+                            <div class="team-member__details-item">
+                                <ul class="aside-socials no-margin-top">
+                                    <li class="aside-socials__item"><a class="aside-socials__link" href="#"><i
+                                                class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                    <li class="aside-socials__item"><a class="aside-socials__link" href="#"><i
+                                                class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                                    <li class="aside-socials__item"><a class="aside-socials__link" href="#"><i
+                                                class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                    <li class="aside-socials__item"><a class="aside-socials__link" href="#"><i
+                                                class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div><a class="button button--primary" href="#">Send Message</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- section end-->
+        <!-- section start-->
+        <section class="section background--brown">
+            <div class="container">
+                <div class="row align-items-end margin-bottom">
+                    <div class="col-sm-6">
+                        <div class="heading heading--primary"><span class="heading__pre-title">What we Did</span>
+                            <h2 class="heading__title no-margin-bottom"><span>Helpo</span> <span>Projects</span></h2>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 text-sm-right">
+                        <!-- slider nav start-->
+                        <div class="slider__nav projects-slider__nav">
+                            <div class="slider__arrows">
+                                <div class="slider__prev"><i class="fa fa-chevron-left" aria-hidden="true"></i>
+                                </div>
+                                <div class="slider__next"><i class="fa fa-chevron-right" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- slider nav end-->
+                    </div>
+                </div>
+            </div>
+            <div class="projects-slider">
+                <div class="projects-slider__item">
+                    <div class="projects-masonry__item projects-masonry__item--primary projects-masonry__item--height-2">
+                        <div class="projects-masonry__img"><img class="img--bg" src="img/projects-slider_1.jpg"
+                                alt="img" />
+                            <div class="projects-masonry__inner"><span class="projects-masonry__badge"
+                                    style="background: #F36F8F;">Medicine</span>
+                                <h3 class="projects-masonry__title"> <a href="cause-details.html">More than One Life
+                                        Changed</a></h3>
+                                <p>Rockweed gunnel; candlefish mail-cheeked fish, yellowtail snapper cuskfish elasmobranch
+                                    seamoth triggerfish gar </p>
+                                <div class="projects-masonry__details-holder">
+                                    <div class="projects-masonry__details-item"><span>Goal: </span><span>25 000$</span>
+                                    </div>
+                                    <div class="projects-masonry__details-item"><span>Date: </span><span>23 Jan'19</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="projects-slider__item">
+                    <div class="projects-masonry__item projects-masonry__item--primary projects-masonry__item--height-2">
+                        <div class="projects-masonry__img"><img class="img--bg" src="img/projects-slider_2.jpg"
+                                alt="img" />
+                            <div class="projects-masonry__inner"><span class="projects-masonry__badge"
+                                    style="background: #49C2DF;">Water Delivery</span>
+                                <h3 class="projects-masonry__title"> <a href="cause-details.html">Helpo for Help</a></h3>
+                                <p>Gray eel-catfish longnose whiptail catfish smalleye squaretail queen danio unicorn fish
+                                    shortnose greeneye fusilier fish silver carp</p>
+                                <div class="projects-masonry__details-holder">
+                                    <div class="projects-masonry__details-item"><span>Goal: </span><span>25 000$</span>
+                                    </div>
+                                    <div class="projects-masonry__details-item"><span>Date: </span><span>23 Jan'19</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="projects-slider__item">
+                    <div class="projects-masonry__item projects-masonry__item--primary projects-masonry__item--height-2">
+                        <div class="projects-masonry__img"><img class="img--bg" src="img/projects-slider_3.jpg"
+                                alt="img" />
+                            <div class="projects-masonry__inner"><span class="projects-masonry__badge"
+                                    style="background: #F36F8F;">Medicine</span>
+                                <h3 class="projects-masonry__title"> <a href="cause-details.html">He Needs Your
+                                        Protection</a></h3>
+                                <p>Sea snail bigeye zebra turkeyfish mola mola sunfish roach. Southern Dolly Varden,
+                                    lightfish ray long-whiskered catfish; burbot dealfish</p>
+                                <div class="projects-masonry__details-holder">
+                                    <div class="projects-masonry__details-item"><span>Goal: </span><span>35 000$</span>
+                                    </div>
+                                    <div class="projects-masonry__details-item"><span>Date: </span><span>23 Jan'19</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="projects-slider__item">
+                    <div class="projects-masonry__item projects-masonry__item--primary projects-masonry__item--height-2">
+                        <div class="projects-masonry__img"><img class="img--bg" src="img/projects-slider_2.jpg"
+                                alt="img" />
+                            <div class="projects-masonry__inner"><span class="projects-masonry__badge"
+                                    style="background: #F36F8F;">Medicine</span>
+                                <h3 class="projects-masonry__title"> <a href="cause-details.html">More than One Life
+                                        Changed</a></h3>
+                                <p>Rockweed gunnel; candlefish mail-cheeked fish, yellowtail snapper cuskfish elasmobranch
+                                    seamoth triggerfish gar </p>
+                                <div class="projects-masonry__details-holder">
+                                    <div class="projects-masonry__details-item"><span>Goal: </span><span>25 000$</span>
+                                    </div>
+                                    <div class="projects-masonry__details-item"><span>Date: </span><span>23 Jan'19</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- section end-->
+        <!-- bottom bg start-->
+        <section class="bottom-background background--brown">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="bottom-background__img"><img src="img/bottom-bg.png" alt="img" /></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- bottom bg end-->
+    </main>
+@endsection
